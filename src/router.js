4fb020f38cdade593ca41564d23c7c974a856b67
Cindy Wong <cindywongdev@gmail.com>
2023-01-03 20:04:50 -0500

    connected main, currencies, and price pages as routes to app

--- src/router.js
+++ src/router.js
@@ -4,15 +4,36 @@ import {
     createRoutesFromElements,
     Route
 } from 'react-router-dom'
+import App from './App'
+import Main from './pages/main'
+import Currencies from './pages/currencies'
+import Price from './pages/price'
 
 // create the browser router
 // this method is ALWAYS first
 const router = createBrowserRouter(
     // helper router INSIDE first method
     // accepts elements as arguments and creates routes from them
-    createRoutesFromElements(
+    createRoutesFromElements(<>
         // Route is a predefined element that we imported above from react-router-dom
-        <Route path='/' element={<h1>Hello World</h1>}></Route>
+        <Route 
+            path='/' 
+            element={<App/>}
+        >
+            <Route
+                path=''
+                element={<Main/>}
+            />  
+            <Route
+                path='currencies'
+                element={<Currencies/>}
+            />
+            <Route
+                path='price'
+                element={<Price/>}
+            />
+        </Route>
+        </>
     )
 )
 
